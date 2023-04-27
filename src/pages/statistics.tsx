4a8a3eb9ab5a1800617e9358741fe5e638a3bf60
grasshopper-dyl <94dwalsh@gmail.com>
2023-04-27 02:03:58 -0500

    h screen

--- src/pages/statistics.tsx
+++ src/pages/statistics.tsx
@@ -5,7 +5,7 @@ function statistics() {
   return (
 <>
 <Layout>
-<div className='w-full h-full bg-inherit flex justify-center items-center'>
+<div className='w-full h-screen bg-inherit flex justify-center items-center'>
   <h1 className='p-8 bg-zinc-800 text-white text-xl'>Statistics</h1>
 </div>
 </Layout>
