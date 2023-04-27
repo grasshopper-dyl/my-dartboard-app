68a65dbd3c2783f0a8f7d1efd4ccd4f9d9541559
grasshopper-dyl <94dwalsh@gmail.com>
2023-04-27 01:31:50 -0500

    added type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
+const App: React.FC<AppProps> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
 }
+
+export default App;
