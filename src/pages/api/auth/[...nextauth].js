691bed60ed838c7d45e9a3b8e2269e480c9b0451
grasshopper-dyl <94dwalsh@gmail.com>
2023-05-02 11:57:45 -0500

    fixing auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,15 +1,13 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-
 export default NextAuth({
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        allowDangerousEmailAccountLinking: true,
-      }),
-    ],
-    // Add any other NextAuth.js configuration options here
-  });
-  
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
+    }),
+  ],
+  // Add any other NextAuth options or callbacks here
+});
